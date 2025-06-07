1a9b7ac933658c84d1776a3a867eb671a341a392
DeVante Pickering <devante@pick2solutions.com>
2025-06-06 23:01:17 -0400

    add versions.tf

---
+++ versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+       version = ">= 4.29.0"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
