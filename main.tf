8a8af4ea6272bf5030b5ac28900398199694ebc9
DeVante Pickering <devante@pick2solutions.com>
2025-06-06 22:56:57 -0400

    add resource group.

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+resource "azurerm_resource_group" "storage_backup_demo" {
+  name     = "storage-backup-demo-rg"
+  location = "eastus"
+}
+
 module "storage_account_with_backup_protection" {
   source = "git::https://github.com/pick2solutions/pick2-iac-core.git//azure/storage_account_with_backup_protection?ref=feat-storage-with-backup"
 
   storage_account_name         = "pick2storageacct"
-  resource_group_name          = "storage-backup-demo-rg"
-  location                     = "eastus"
+  resource_group_name          = azurerm_resource_group.storage_backup_demo.name
+  location                     = azurerm_resource_group.storage_backup_demo.location
   file_share_name              = "pick2files"
   file_share_quota             = 100
   account_tier                 = "Standard"
